f52b6a7260d18b985833f4e31c96f2ae5456d7d2
marshvin <vincentmarsh01@gmail.com>
2024-01-01 21:27:29 +0300

    new

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
 const Card = ({ image, title, paragraph }) => {
-    return (
-      <div className="w-full sm:w-96 md:w-96 lg:w-96 p-4" style={{ height: '100vh' }}>
-        <div className="bg-cover bg-center h-80 sm:h-96 rounded-lg shadow-md" style={{ backgroundImage: `url(${image})` }}>
-          <div className="bg-opacity-60 h-full flex flex-col justify-end p-4">
-            <h2 className="text-white text-xl font-semibold mb-2">{title}</h2>
-            <p className="text-white text-sm mb-2">{paragraph}</p>
-            <div className="flex flex-row space-x-2">
-              <button className="text-white font-semibold py-2 px-4 rounded border border-red-500 hover:bg-red-500 hover:text-white transition-colors">
-                View
-              </button>
-              <button className="text-white font-semibold py-2 px-4 rounded hover:text-red-500 hover:underline transition-colors">
-                Learn More
-              </button>
-            </div>
+  return (
+    <div className="w-full sm:w-96 md:w-96 lg:w-96 p-4">
+      <div className="bg-cover bg-center h-80 sm:h-96 rounded-lg shadow-md" style={{ backgroundImage: `url(${image})` }}>
+        <div className="bg-opacity-60 h-full flex flex-col justify-end p-4">
+          <h2 className="text-white text-xl font-semibold mb-2">{title}</h2>
+          <p className="text-white text-sm mb-2">{paragraph}</p>
+          <div className="flex flex-row space-x-2">
+            <button className="text-white font-semibold py-2 px-4 rounded border border-red-500 hover:bg-red-500 hover:text-white transition-colors">
+              View
+            </button>
+            <button className="text-white font-semibold py-2 px-4 rounded hover:text-red-500 hover:underline transition-colors">
+              Learn More
+            </button>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 const CardList = () => {
   const cards = [
@@ -41,12 +41,10 @@ const CardList = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <p className="text-2xl text-gray-500 text-center mb-6">What have I done ?</p>
-        <h2 className="text-3xl text-center font-bold mb-6">
-          My Projects
-        </h2>
-      <div className="flex flex-wrap">
+    <div className="min-h-screen bg-gray-100 p-4 flex flex-col items-center">
+      <p className="text-2xl text-gray-500 mb-6">What have I done ?</p>
+      <h2 className="text-3xl font-bold mb-6">My Projects</h2>
+      <div className="flex flex-wrap justify-center">
         {cards.map((card, index) => (
           <Card key={index} {...card} />
         ))}
