5cd5df384f16199968445e8b60835ee99b6935bd
marshvin <vincentmarsh01@gmail.com>
2024-02-06 23:19:03 +0300

    Added better cards on blog component

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -2,50 +2,31 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 flex flex-col items-center justify-center h-full py-10">
       <p className="text-2xl text-gray-500 text-center mb-6">Learn Something </p>
-      <h2 className="text-3xl text-center font-bold mb-6">
-        My Blogs
-      </h2>
-      <div className="flex flex-wrap  justify-center">
-        {/* First Card */}
-        <div className="relative flex bg-clip-border rounded-xl bg-white text-gray-700 shadow-md w-full max-w-[48rem] flex-row m-4">
-          <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
-            <img
-              src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
-              alt="card-image"
-              className="object-cover w-full h-full"
-            />
-          </div>
-          <div className="p-6">
-            <h6 className="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700 uppercase">
-              Postgresql
-            </h6>
-            <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-              How to Store Images in Database
-            </h4>
-            <p className="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
-              Like so many organizations these days, Autodesk is a company in transition. It was until recently a traditional boxed software company selling licenses. Yet its own business model disruption is only part of the story.
-            </p>
-            <a href="#" className="inline-block">
-              <button
-                className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/10 active:bg-gray-900/20"
-                type="button"
-              >
-                Learn More
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" className="w-4 h-4">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path>
-                </svg>
-              </button>
-            </a>
+      <h2 className="text-3xl text-center font-bold mb-6">My Blogs</h2>
+      
+      {/* First Card */}
+      <a href="#" className="flex items-center justify-center bg-white border border-gray-200 rounded-lg shadow-md mb-6 w-3/4 lg:w-2/3 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div className="flex flex-col lg:flex-row">
+          <img className="object-cover w-full h-64 lg:h-auto lg:w-48 lg:rounded-l-lg lg:rounded-t-none" src="images/aws2.png" alt="" />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">How to store Images on AWS S3 Using flask</h5>
+            <p className="mb-3 text-sm lg:text-base font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
           </div>
         </div>
+      </a>
 
-        {/* Second Card  */}
-        <div className="relative flex bg-clip-border rounded-xl bg-white text-gray-700 shadow-md w-full max-w-[48rem] flex-row m-4">
-        
+      {/* Second Card */}
+      <a href="#" className="flex items-center justify-center bg-white border border-gray-200 rounded-lg shadow-md mb-6 w-3/4 lg:w-2/3 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div className="flex flex-col lg:flex-row">
+          <img className="object-cover w-full h-64 lg:h-auto lg:w-48 lg:rounded-l-lg lg:rounded-t-none" src="images/AI.png" alt="" />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cotinuous learning for Robotics using AI</h5>
+            <p className="mb-3 text-sm lg:text-base font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
